0b5774c26d96a976d882acee6c24e15ea260dca6
tahasyed11 <150862872+tahasyed11@users.noreply.github.com>
2024-03-07 11:52:32 -0500

    Added ufo animation

--- src/CityScape.java
+++ src/CityScape.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 import javax.swing.*;
 public class CityScape extends JPanel {
 
-    private static int width = 1020;
+    public static int width = 1020;
     private static int height = 640;
     private Building[] b = new Building[8];
+    private Ufo[] ufos = new Ufo[5];
 
     private ArrayList<Integer[]> stars = new ArrayList<>();
 
@@ -22,14 +23,28 @@ public class CityScape extends JPanel {
         for (int i = 0; i < 150; i++) {
             stars.add(new Integer[]{(int) Math.floor(Math.random() * width), (int) Math.floor(Math.random() * height)});
         }
+
+        ufos[0] = new Ufo(0, 0);
+        ufos[1] = new Ufo(300, 20);
+        ufos[2] = new Ufo(5, 300);
+        ufos[3] = new Ufo(250, 250);
+        ufos[4] = new Ufo(70, 2);
+    }
+
+    public void move() {
+        for (Ufo u : ufos) {
+            u.move();
+        }
     }
 
     @Override
     public void paint(Graphics g) {
+        super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         g2d.setColor(Color.BLACK);
         g2d.fillRect(0, 0, width, height);
-
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         //stars
         g2d.setColor(Color.WHITE);
         for (Integer[] arr : stars) {
@@ -49,9 +64,13 @@ public class CityScape extends JPanel {
             k.paint(g2d);
         }
 
+        for (Ufo u : ufos) {
+            u.paint(g2d);
+        }
+
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         JFrame frame = new JFrame("My Drawing");
 
         CityScape cityScape = new CityScape();
@@ -60,5 +79,12 @@ public class CityScape extends JPanel {
         frame.setSize(width, height);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        while (true)
+        {
+            cityScape.move();
+            cityScape.repaint();
+            Thread.sleep(10);
+        }
     }
 }
--- src/Ufo.java
+++ src/Ufo.java
@@ -4,13 +4,30 @@ public class Ufo {
 
     private int x;
     private int y;
+    private int dx = 1;
+    private int dy = 1;
 
     public Ufo(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public void paint(Graphics2D g2d) {
+    public void move() {
+        if (x + dx < 0) {
+            dx = 1;
+        } else if (x + dx + 30 > CityScape.width) {
+            dx = -1;
+        } else if (y + dy < 0) {
+            dy = 1;
+        } else if (y + dy + 30 > 200) {
+            dy = -1;
+        }
+        x += dx;
+        y += dy;
+    }
 
+    public void paint(Graphics2D g2d) {
+        g2d.setColor(Color.YELLOW);
+        g2d.fillOval(x, y, 30, 30);
     }
 }
